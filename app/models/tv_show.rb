ac369df240fe5a3b2997933c920962b16996ea1d
Sam Cavenagh <sam.cavenagh@gmail.com>
2010-03-30 14:14:18 +1100

    group series on show page

--- app/models/tv_show.rb
+++ app/models/tv_show.rb
@@ -1,5 +1,14 @@
 class TvShow < VideoContent
-  has_many :tv_episodes
+  has_many :tv_episodes, :order => 'series, episode' do
+    def group_by_series
+      result = {}
+      self.each do |episode|
+        result[episode.series] = [] unless result[episode.series]
+        result[episode.series] << episode
+      end  
+      return result
+    end  
+  end  
   
   def tv_show?
     true
