aa553068e7352c82d841b4662ec30685489f611e
Sam Cavenagh <sam.cavenagh@gmail.com>
2010-05-02 14:49:50 +1000

    clean up home page js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function changeTooltip(element_id, new_message){
+	eval('var tt_var = var_tt_' + element_id)			
+	tt_var.destroy();
+	tt_var = new YAHOO.widget.Tooltip("tt_" + element_id, { context: element_id, text: new_message });
+	eval('var_tt_' + element_id + ' = tt_var')
+}
---
+++ public/javascripts/home.js
@@ -0,0 +1,84 @@
+function VideoCarousel (container, videoCount, carWidth, carHeight, loadUrl) {
+    var carousel;
+    
+    function init(){
+  	    var carouselState = YAHOO.util.History.getBookmarkedState("carouselState");
+		    var initialCarouselState = carouselState || "0";
+			
+			YAHOO.util.History.register("carouselState", initialCarouselState, function (state) {
+				setCarouselFirstVisible(parseInt(state));
+		    });
+			
+			YAHOO.util.History.onReady(function () {
+		        var currentState = YAHOO.util.History.getCurrentState("carouselState");
+				YAHOO.log("Init carousel with state: " + currentState);				
+		        initCarousel(parseInt(currentState));
+		    });
+			
+		    // Initialize the browser history management library.
+		    try {
+		        YAHOO.util.History.initialize("yui-history-field", "yui-history-iframe");
+		    } catch (e) {
+		        YAHOO.log("Unable to init carousel history: " + e);
+		        initCarousel(0);
+		    }  
+    }
+    YAHOO.util.Event.onDOMReady(init);
+    
+		function initCarousel(firstVisible) {
+		    carousel = new YAHOO.widget.Carousel(container, {
+		        animation: { speed: 0.5, effect: YAHOO.util.Easing.easeOut },
+		        numVisible: [carWidth,carHeight],
+				numItems: videoCount
+		    });
+		
+			carousel.on("loadItems", loadCarouselItems);
+			carousel.on("pageChange", recordCarouselHistory);
+		  carousel.render();
+			carousel.show();
+			setCarouselFirstVisible(firstVisible);   			
+		}
+		
+		function loadCarouselItems(o) {
+			var offset = 0;
+			var pageSize = carWidth*carHeight;
+	        if(o){
+	            YAHOO.log('Num: ' + o.num + " First: " + o.first + " Last: " + o.last);   
+	            offset = o.first;
+				pageSize = o.num;
+	        }
+
+			new Ajax.Request(loadUrl, { 
+				method:'get', 
+				parameters: {'offset': offset, 'page_size': pageSize},
+				evalJS:'force' 
+			});		
+		}
+	
+		function addCarouselItem(item){
+			carousel.addItem(item);
+		}
+		this.addCarouselItem = addCarouselItem;
+		
+		function recordCarouselHistory(e){
+	        var newState = carousel.get('firstVisible');
+	        try {
+	            var currentState = YAHOO.util.History.getCurrentState("carouselState");
+				//YAHOO.log("Carousel new: " + newState + " current: " + currentState);
+	            if (newState != currentState) {
+	                YAHOO.util.History.navigate("carouselState", "" + newState);
+	            }
+	        } catch (e) {
+	            YAHOO.log("Error recording carousel history: " + e);
+	        }			
+		}
+		
+		//Sets the first visible without any animiation
+		function setCarouselFirstVisible(state){
+			var animation = carousel.get('animation');
+			carousel.set('animation', {speed: 0, effect: null});
+			carousel.set("firstVisible", state);
+			carousel.set('animation', animation);
+		}    
+    
+}
