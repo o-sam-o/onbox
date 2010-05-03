2bcd0776cc06a7c501c05766516c75e40b5c0fbe
Sam Cavenagh <sam.cavenagh@gmail.com>
2010-05-03 21:46:47 +1000

    clean up js on show video content page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,3 +7,35 @@ function changeTooltip(element_id, new_message){
 	tt_var = new YAHOO.widget.Tooltip("tt_" + element_id, { context: element_id, text: new_message });
 	eval('var_tt_' + element_id + ' = tt_var')
 }
+
+function addWatchClickListener(elementId, videoId, newWatchedValue) {
+    YAHOO.util.Event.removeListener(elementId, 'click');
+    YAHOO.util.Event.addListener(elementId, "click", function(){markVideoWatched(videoId, newWatchedValue); return false;});
+}
+
+function markVideoWatched(videoId, watched){
+	if (!isUserLoggedIn()){
+		window.location = "/login";
+		return;
+	}
+	new Ajax.Request('/video_contents/' + videoId + '/watch',
+	  {
+	    method:'post',
+		  parameters: {watched: watched},
+	    onSuccess: function(transport){
+  	    if(transport.responseText == "true"){
+    			if(watched){
+    				YAHOO.util.Dom.removeClass('watchedWrapper', 'notWatched'); 
+    				changeTooltip('watchedContentLink', 'Watched');
+    			}else{
+    				YAHOO.util.Dom.addClass('watchedWrapper', 'notWatched'); 
+    				changeTooltip('watchedContentLink', 'Click to mark as watched');
+    			}
+    			addWatchClickListener('watchedContentLink', videoId, !watched);
+    	  }else{
+    			alert('Error marking video watched: ' + transport.responseText)
+    		}
+	    },
+	    onFailure: function(){ alert('Failed to mark video as watched.') }
+	  });
+}
